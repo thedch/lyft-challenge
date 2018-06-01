e2ce0ecf0048729e3096080ee5ab2dc6c2e37c5c
Daniel Hunter <officialdanielhunter@gmail.com>
2018-06-01 17:18:46 +0000

    Modify submit.py to handle two different models (temp fix, will clean it up soon), and add .pt files to gitingore

--- submit.py
+++ submit.py
@@ -25,8 +25,6 @@ from fastai.transforms import *
 import torch
 import cv2
 
-sz = 128
-
 class StdUpsample(nn.Module):
     def __init__(self, nin, nout):
         super().__init__()
@@ -35,9 +33,10 @@ class StdUpsample(nn.Module):
         
     def forward(self, x): return self.bn(F.relu(self.conv(x)))
 
+def grab_last_channel(x): return x[:,0]
 def not_lambda(x): return x[:,0] # TODO: Rename this please
 
-flatten_channel = Lambda(not_lambda)
+flatten_channel = Lambda(grab_last_channel)
 
 simple_up = nn.Sequential(
     nn.ReLU(),
@@ -52,31 +51,36 @@ simple_up = nn.Sequential(
 aug_tfms = [RandomRotate(4, tfm_y=TfmType.CLASS),
             RandomFlip(tfm_y=TfmType.CLASS),
             RandomLighting(0.05, 0.05)]
-transforms = tfms_from_model(resnet34, sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
-train_tfms, val_tfms = transforms
+small_sz = 128
+big_sz = 512
+big_transforms = tfms_from_model(resnet34, big_sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
+small_transforms = tfms_from_model(resnet34, small_sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
+# train_tfms, val_tfms = transforms
 
 road_learn = torch.load('road-fullmodel.pt')
-car_learn = torch.load('car-fullmodel.pt')
+car_learn = torch.load('car-fullmodel-3.pt')
 
 # Frame numbering starts at 1
 frame = 1
 
 for im in video:
+    im = im.astype(np.float32)/255
     
-    aug_t, aug_v = val_tfms(im, im)
+    # Road section (128)
+    aug_t, aug_v = small_transforms[1](im, im)
     im_with_batch = V(aug_t).unsqueeze_(0)
-    
-    # Road section
     pred = road_learn(im_with_batch)
     pred_np = to_np(pred)
     pred_big = cv2.resize(pred_np[0], (800, 600)) 
-    binary_road_result = np.where(pred_big>0,1,0).astype('uint8')
+    binary_road_result = np.where(pred_big>3,1,0).astype('uint8')
     
-    # Car section
+    # Car section (512)
+    aug_t, aug_v = big_transforms[1](im, im)
+    im_with_batch = V(aug_t).unsqueeze_(0)
     pred = car_learn(im_with_batch)
     pred_np = to_np(pred)
     pred_big = cv2.resize(pred_np[0], (800, 600)) 
-    binary_car_result = np.where(pred_big>0,1,0).astype('uint8')
+    binary_car_result = np.where(pred_big>-2,1,0).astype('uint8')
 
     # Save answers
     answer_key[frame] = [encode(binary_car_result), encode(binary_road_result)]        
