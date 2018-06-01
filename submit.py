ad75ede28fbee01180c863a2d71b9a033c44e4fa
Daniel Hunter <officialdanielhunter@gmail.com>
2018-06-01 21:29:43 +0000

    Clean up submit file

--- submit.py
+++ submit.py
@@ -21,33 +21,29 @@ answer_key = {}
 aug_tfms = [RandomRotate(4, tfm_y=TfmType.CLASS),
             RandomFlip(tfm_y=TfmType.CLASS),
             RandomLighting(0.05, 0.05)]
-small_sz = 128
-big_sz = 512
-big_transforms = tfms_from_model(resnet34, big_sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
-small_transforms = tfms_from_model(resnet34, small_sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
-# train_tfms, val_tfms = transforms
+
+sz = 512
+transforms = tfms_from_model(resnet34, sz, crop_type=CropType.NO, tfm_y=TfmType.CLASS, aug_tfms=aug_tfms)
 
 road_learn = torch.load('road-fullmodel.pt')
 car_learn = torch.load('car-fullmodel-3.pt')
 
-# Frame numbering starts at 1
-frame_num = 1
+frame_num = 1 # Frame numbering starts at 1
     
 video = skvideo.io.vread(file)        
 for im in video:
     im = im.astype(np.float32)/255
     
-    # Road section (128)
-    aug_t, aug_v = small_transforms[1](im, im)
+    aug_t, aug_v = transforms[1](im, im)
     im_with_batch = V(aug_t).unsqueeze_(0)
+    
+    # Road section    
     pred = road_learn(im_with_batch)
     pred_np = to_np(pred)
     pred_big = cv2.resize(pred_np[0], (800, 600)) 
     binary_road_result = np.where(pred_big>3,1,0).astype('uint8')
     
-    # Car section (512)
-    aug_t, aug_v = big_transforms[1](im, im)
-    im_with_batch = V(aug_t).unsqueeze_(0)
+    # Car section        
     pred = car_learn(im_with_batch)
     pred_np = to_np(pred)
     pred_big = cv2.resize(pred_np[0], (800, 600)) 
